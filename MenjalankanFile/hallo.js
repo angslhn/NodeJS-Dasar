aa9c476b90ecc92bed9c584db54d6cbece5746f5
angslhn <angslhn@gmail.com>
2024-11-18 20:00:07 +0700

    Menjalankan file lain.

---
+++ MenjalankanFile/hallo.js
@@ -0,0 +1,5 @@
+function ucapanSalam(nama) {
+    return `Hallo selamat malam ${nama}`;
+}
+
+module.exports = ucapanSalam;
---
+++ MenjalankanFile/runfile.js
@@ -0,0 +1,3 @@
+const ucapanSalam = require('./hallo');
+
+console.log(ucapanSalam('Aang Solihin'));
